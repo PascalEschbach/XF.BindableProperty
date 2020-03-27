d3d6f0d73184d47f7c57f9500f5ef641840331a2
Pascal Eschbach <heroicsoft@web.de>
2020-03-27 08:36:36 +0100

    Added extensions

---
+++ XF.BindableProperty.Fody/Extensions/Instructions/InstructionLinker.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil.Cil;
+
+public static class InstructionLinker {
+
+	public static IEnumerable<Instruction> Copy( IEnumerable<Instruction> instructions ) {
+
+		//Copy body
+		var originalBody = instructions.ToList();
+		var body = originalBody.Select( il => il.Copy() ).ToArray();
+
+		Link( body );
+
+		//Redirect jumps
+		foreach( var instruction in body ) {
+
+			if( instruction.Operand is Instruction jump ) 
+				instruction.Operand = body[originalBody.IndexOf( jump )];
+
+			if( instruction.Operand is Instruction[] jumpList )
+				instruction.Operand = jumpList.Select( j => body[originalBody.IndexOf( j )] ).ToArray();
+		}
+
+		return body;
+	}
+	public static IEnumerable<Instruction> Link( IEnumerable<Instruction> instructions ) {
+		var body = instructions.ToArray();
+		for( int i = 0; i < body.Length; i++ ) {
+			if( i > 0 )
+				body[i].Previous = body[i - 1];
+			if( i < body.Length - 1 )
+				body[i].Next = body[i + 1];
+		}
+
+		return body.ToArray();
+	}
+
+
+	public static Instruction[] Add( this Instruction[] instructions, Instruction newIL ) {
+		if( instructions is null || !instructions.Any() )
+			return new [] { newIL };
+
+		var copy = new List<Instruction>( instructions ) { newIL }.ToArray();
+
+		copy[copy.Length - 2].Next = newIL;
+		newIL.Previous = copy[copy.Length - 2];
+
+		return copy;
+	}
+	public static Instruction[] Remove( this Instruction[] instructions, Instruction il ) {
+		if( instructions is null || !instructions.Any() )
+			return instructions;
+
+		var list = new List<Instruction>( instructions );
+		if( !list.Remove( il ))
+			return list.ToArray();
+
+		if( il.Previous != null )
+			il.Previous.Next = il.Next;
+		if( il.Next != null )
+			il.Next.Previous = il.Previous;
+
+		return list.ToArray();
+	}
+	public static void Set( this Instruction[] instructions, int index, Instruction il ) {
+		var oldIL = instructions[index];
+
+		instructions[index] = il;
+		il.Next = oldIL.Next;
+		il.Previous = oldIL.Previous;
+
+		if( il.Next != null )
+			il.Next.Previous = il;
+		if( il.Previous != null )
+			il.Previous.Next = il;
+	}
+	public static int IndexOf( this Instruction[] instructions, Instruction il )
+		=> new List<Instruction>( instructions ).IndexOf( il );
+}
+
---
+++ XF.BindableProperty.Fody/Extensions/LinqExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Linq {
+
+	public static class LinqExtension {
+
+		public static void ForEach<T>( this IEnumerable<T> source, Action<T> action ) {
+			foreach( T item in source )
+				action( item );
+		}
+		public static IEnumerable<R> ForEach<T, R>( this IEnumerable<T> source, Func<T, R> func ) {
+			List<R> results = new List<R>();
+			foreach( T item in source )
+				results.Add( func( item ) );
+
+			return results;
+		}
+
+		public static TSource[] ToArray<TSource>( this IEnumerable<TSource> source, Func<TSource, int, bool> predicate )
+			=> source.Where( predicate ).ToArray();
+		public static TSource[] ToArray<TSource>( this IEnumerable<TSource> source, Func<TSource, bool> predicate )
+			=> source.Where( predicate ).ToArray();
+		public static TResult[] ToArray<TSource, TResult>( this IEnumerable<TSource> source, Func<TSource, TResult> selector )
+			=> source.Select( selector ).ToArray();
+		public static TResult[] ToArray<TSource, TResult>( this IEnumerable<TSource> source, Func<TSource, int, TResult> selector )
+			=> source.Select( selector ).ToArray();
+	}
+}
